2cfd48bc2dae08a8ea6222be771fe1349289c8be
Bukz-cloud <bukadejohn@gmail.com>
2025-11-15 19:38:55 +0200

    place.html, place.css, place-large.css, and place.js files were created to complete the assignment.

---
+++ scripts/place.js
@@ -0,0 +1,32 @@
+// WINDCHILL CALCULATION
+function calculateWindChill(tempC, windKmh) {
+    // Wind chill formula for °C
+    return (
+        13.12 +
+        0.6215 * tempC -
+        11.37 * Math.pow(windKmh, 0.16) +
+        0.3965 * tempC * Math.pow(windKmh, 0.16)
+    ).toFixed(1);
+}
+
+function displayWindChill() {
+    const temp = parseFloat(document.getElementById("temp").textContent);
+    const wind = parseFloat(document.getElementById("wind").textContent);
+    const output = document.getElementById("windchill");
+
+    if (temp <= 10 && wind > 4.8) {
+        output.textContent = calculateWindChill(temp, wind) + "°C";
+    } else {
+        output.textContent = "N/A";
+    }
+}
+
+displayWindChill();
+
+// LAST MODIFIED FOOTER
+//document.getElementById("lastModified").textContent = document.lastModified;
+
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = `${currentYear}`;
+document.getElementById("lastModified").innerHTML = `Last modified: ${document.lastModified}`;
